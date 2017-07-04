972e0906205c35b734f3b1679273b6d440b8877d
He Xing Darius <E0007888@u.nus.edu>
2017-07-04 11:40:15 +0800

    Functions all in place

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ var express = require('express')
 var path = require('path')
 var bodyParser = require('body-parser')
 var app = express()
+var port = 3000
+var result
 
 app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'views/index.html'));
@@ -19,21 +23,20 @@ var storage = multer.diskStorage({
     filename: function(req, file, callback){
         callback(null, new Date() + "-" + file.fieldname)
     }
+});
+
+app.post('/', multer({dest: './storage/'}).single('myFile'), function(req, res){
+    console.log(req.file);
+    result = { "size" : req.file.size }
+    res.redirect('/filesize');
 })
 
-var upload = multer({dest: './storage/'}).single('myFile');
-
-app.post('/upload', function(req, res){
-    upload(req, res, function(err){
-        if(err){
-            return res.send('Error uploading');
-        }
-        res.end('uploading successful')
-    })
+app.get('/filesize', function(req, res){
+    res.json(result)
 })
 
 /*----------------------------Multer--------------------------------*/
 
-var server = app.listen(process.env.PORT || 3000, function(){
-    console.log("Server listening on port " + ((process.env.PORT)? process.env.PORT : '3000'))
+var server = app.listen(process.env.PORT || port, function(){
+    console.log("Server listening on port " + ((process.env.PORT)? process.env.PORT : port))
 })
