44230b093911691dd182ce126693e90f3d619928
He Xing Darius <E0007888@u.nus.edu>
2017-07-05 11:04:54 +0800

    Enhenced function: include multiple file uploads

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ var path = require('path')
 var bodyParser = require('body-parser')
 var app = express()
 var port = 3000
-var result
+var result = []
+//var file
 
 app.use(bodyParser.json())
 
@@ -16,22 +17,43 @@ app.get('/', function(req, res){
 
 /*----------------------------Multer--------------------------------*/
 
+/* Not specifiying a storage path such that user uploaded files WON'T be stored.
 var storage = multer.diskStorage({
     destination: function(req, file, callback){
-        callback(null, './storage');
+        callback(null, 'storage/');
     },
     filename: function(req, file, callback){
         callback(null, new Date() + "-" + file.fieldname)
     }
 });
+*/
 
-app.post('/', multer({dest: './storage/'}).single('myFile'), function(req, res){
-    console.log(req.file);
-    result = { "size" : req.file.size }
-    res.redirect('/filesize');
+var upload = multer({})
+
+app.post('/multiple', upload.array('files', 4), function(req, res){
+    console.log(req.files);
+
+    for(var i = 0; i < req.files.length; i++){
+        result[i] = { "name": "item " + i, "size" : req.files[i].size }
+    } 
+
+    res.redirect('/collectionsize')
 })
 
+/* Multer's upload function cannot have the same path, otherwise the 2nd one wont work. But why? */
+app.post('/single', upload.single('myFile'), function(req, res){
+    console.log(req.file);
+    result = { "size" : req.file.size}
+    file = req.file
+    res.redirect('/filesize');
+})
 app.get('/filesize', function(req, res){
+    res.json(result)
+    //res.json(file) was trying to display this but the raw file data file in json will be too overwhelming
+})
+
+app.get('/collectionsize', function(req, res){
+    
     res.json(result)
 })
 
